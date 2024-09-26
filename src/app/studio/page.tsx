35091d1a8070a40a3c8329f06d55eccf81aeb0c6
RTS <RTS@melosh.tech>
2024-09-26 12:51:07 +0200

    test watchtower

--- src/app/studio/page.tsx
+++ src/app/studio/page.tsx
@@ -1,8 +1,17 @@
+import { HydrateClient } from "~/trpc/server";
+import Navbar from "../_components/Navbar";
+import Footer from "../_components/Footer";
+
 export default function StudioPage() {
 
     return (
-        <div>
-            <h1>Studio</h1>
-        </div>
+        <HydrateClient>
+        <Navbar />
+        <main className="flex-grow items-center justify-center dark:bg-black">
+          <h1>STUDIO</h1>
+          <h2>HUGE CHANGES</h2>
+        </main>
+        <Footer/>
+      </HydrateClient>
     )
 }
